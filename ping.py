741543f316cab6774775710440d06dde8633ddf7
Vignesh Ravi <68957716+iam-vignesh@users.noreply.github.com>
2021-04-28 16:37:47 +0530

    Add files via upload

---
+++ ping.py
@@ -0,0 +1,24 @@
+import os
+import csv
+
+
+print("+------------------------------------------------------------------+")
+with open('C:\\Users\\User\\Desktop\\iplist.csv', newline='') as csvfile:
+    filereader = csv.reader(csvfile)
+    next(filereader)
+    for row in filereader:
+        ips = (row[0])
+        print(f"Pinging....{ips}")
+        ping_response = os.popen(f"ping {ips}").read()
+        if "Received = 4" in ping_response:
+            print(f"{ips} is UP. Ping Successful!")
+        else:
+            print(f"Ping to {ips} failed...")
+            down_ip = []
+            down_ip.append(ips)
+            with open('down iplist.csv', 'a' , newline='') as writefile:
+                w = csv.writer(writefile)
+                w.writerow(down_ip)
+        print("+------------------------------------------------------------------+")
+
+
